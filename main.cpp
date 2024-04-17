f932960c5eef9e89f7f7f7b0865e957dbcbec56d
anastasiyanovikova <34402531+anastasiyanovikova@users.noreply.github.com>
2024-04-17 11:43:14 +0300

    комментарии, правки вывода

--- main.cpp
+++ main.cpp
@@ -4,44 +4,34 @@
 
 int main(int, char const **)
 {
-    {
-        // std::map
-        std::map<int, int> stdMap;
-        for(int i = 0; i < 10; ++i)
-            stdMap.insert({i, getFuctorial(i)});
+    // 1.создание экземпляра std::map<int, int>
+    std::map<int, int> stdMap;
+    for(int i = 0; i < 10; ++i)
+        stdMap.insert({i, getFuctorial(i)});
+    for (const auto& n : stdMap)
+        std::cout << n.first << " " << n.second << "; " << std::endl;
 
-        for(auto it = stdMap.begin(); it != stdMap.end(); ++it)
-            std::cout << it->first << " " << it->second << std:: endl;
-    }
-    {
-        // std::map with MyAllocator
-        std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>>> allocMap;
-        for(int i = 0; i < 10; ++i)
-            allocMap.insert({i, getFuctorial(i)});
+    // 2. создание экземпляра std::map<int, int> с новым аллокатором
+    std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>>> allocMap;
+    for(int i = 0; i < 10; ++i)
+        allocMap.insert({i, getFuctorial(i)});
+    for (const auto& n : allocMap)
+        std::cout << n.first << " " << n.second << "; " << std::endl;
 
-        for(auto it = allocMap.begin(); it != allocMap.end(); ++it)
-            std::cout << it->first << " " << it->second << std:: endl;
-    }
-    {
-        // my container
-        MyVector<int> vec;
-        for(int i = 0; i < 10; ++i)
-            vec.push_back(i);
-        
-        for(std::size_t i= 0 ; i < vec.size(); ++i)
-            std::cout << vec[i] << " ";
-        std::cout << std::endl;
-    }
+    // 3. создание экземпляра своего контейнера для хранения значений типа int
+    MyVector<int> vec;
+    for(int i = 0; i < 10; ++i)
+        vec.push_back(i);
+    for(std::size_t i= 0 ; i < vec.size(); ++i)
+        std::cout << vec[i] << " ";
+    std::cout << std::endl;
 
-    {
-        // my container with MyAllocator
-        MyVector<int, MyAllocator<int>> vec;
-        for(int i = 0; i < 10; ++i)
-            vec.push_back(i);
-        for(std::size_t i= 0 ; i < vec.size(); ++i)
-            std::cout << vec[i] << " ";
-        std::cout << std::endl;
-    }
-
+    // 4. создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором,
+    MyVector<int, MyAllocator<int>> vecAlloc;
+    for(int i = 0; i < 10; ++i)
+        vecAlloc.push_back(i);
+    for(std::size_t i= 0 ; i < vecAlloc.size(); ++i)
+        std::cout << vecAlloc[i] << " ";
+    std::cout << std::endl;
     return 0;
 }
